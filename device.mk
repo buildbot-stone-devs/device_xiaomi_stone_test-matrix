e01eb6a25c38023f25dbe43e0418d88da912062d
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    stone: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -371,6 +371,11 @@ PRODUCT_PACKAGES += \
     TelephonyOverlayStone
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Permissions
