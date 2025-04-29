1c5d071cc21049405a3126070f9bbbc9187b8c1d
mayuresh <mayureshnanal846@gmail.com>
2025-04-29 16:29:42 +0530

    stone: qpr2 changes

--- device.mk
+++ device.mk
@@ -7,9 +7,6 @@
 # AudioFX
 TARGET_EXCLUDES_AUDIOFX := true
 
-# Vendor Dolby
-$(call inherit-product, vendor/dolby/dolby.mk)
-
 # Vendor BCR
 $(call inherit-product, vendor/bcr/bcr.mk)
 
@@ -352,7 +349,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.st \
     com.android.nfc_extras \
     libchrome.vendor \
-    NfcNci \
     SecureElement \
     Tag
 
