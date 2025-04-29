6af3ab319d1289f43fb47ed95112c2c692511848
mayuresh <mayureshnanal846@gmail.com>
2025-04-29 17:24:34 +0530

    stone: update security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -216,7 +216,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2024-11-01
+VENDOR_SECURITY_PATCH := 2025-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
